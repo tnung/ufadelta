c5b09af62a65d8437bbec4e6718b227d93c53676
tnung <91000447+tnung@users.noreply.github.com>
2022-09-05 21:56:20 +0700

    Initial commmit

---
+++ spec/ufadelta-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import UfadeltaView from '../lib/ufadelta-view';
+
+describe('UfadeltaView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
